b2787f7891655afd1a82fac0f0cadfe3ddbafdc4
Alexey Andriyanenko <alexlikescoockies@gmail.com>
2023-06-19 20:10:49 +0300

    fix(app-layout): fix page-container paddings

--- src/shared/components/layout/page-container/page-container.styles.ts
+++ src/shared/components/layout/page-container/page-container.styles.ts
@@ -1,6 +1,8 @@
 import { Box, styled } from "@mui/material";
 
 export const Container = styled(Box)(() => ({
+  display: "flex",
+  flexDirection: "column",
   width: "100%",
   height: "100%",
   overflow: "hidden",
--- src/shared/components/layout/page-content/page-content.styles.ts
+++ src/shared/components/layout/page-content/page-content.styles.ts
@@ -1,8 +1,8 @@
 import { Box, styled } from "@mui/material";
 
 export const Content = styled(Box)(() => ({
+  flex: 1,
   width: "100%",
-  height: "100%",
   padding: "24px 16px 32px 16px",
   overflowY: "auto",
 }));
