463a5db0d2cbb057961a58e005e209cc6b88146c
Alexey Andriyanenko <alexlikescoockies@gmail.com>
2023-07-19 16:38:01 +0300

    fix(profile): change page component declaration due to jest failing

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,18 +1,14 @@
 import React from "react";
 import { Typography } from "@mui/material";
 
-// NOTE: if importing from layout/index.ts is will
-// cause ReferenceError: Cannot access _default before initialization
-import { PageContent } from "src/shared/components/layout/page-content";
+import { PageContent } from "src/shared/components/layout";
 import { Form } from "./form";
 
-const Profile: React.FC = () => {
+export default function () {
   return (
     <PageContent>
       <Typography variant="h3"> Profile </Typography>
       <Form />
     </PageContent>
   );
-};
-
-export default Profile;
+}
